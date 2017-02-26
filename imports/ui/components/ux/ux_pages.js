ab0ccfbd8752d55a0a62115a4c6a5f3c8ece06e8
Peterdraw <afandihore@gmail.com>
2017-02-27 00:18:01 +0700

    implementing metrics-graphs

--- imports/ui/components/ux/ux_pages.js
+++ imports/ui/components/ux/ux_pages.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { d3 } from 'd3';
+import { MG } from 'metrics-graphics';
 // Corresponding html file
 import './ux_pages.html';
 
@@ -8,7 +10,23 @@ Template.ux_index_banner.onCreated(() => {});
 
 Template.ux_index_banner.helpers({});
 
-Template.ux_index_banner.onRendered(() => {});
+Template.ux_index_banner.onRendered(() => {
+  d3.json('data/fake_users1.json', function(data) {
+    data = MG.convert.date(data, 'date');
+    MG.data_graphic({
+      title: "",
+      description: "For single line charts, there are two simple ways to change a line color. The first is to change the css (described on the wiki). The other is to specify a color value using color: string or colors: string.",
+      data: data,
+      full_width: true,
+      height: 250,
+      right: 40,
+      color: '#1189c6',
+      target: '#charts',
+      x_accessor: 'date',
+      y_accessor: 'value'
+    });
+  });
+});
 
 Template.ux_index_banner.events({});
 
